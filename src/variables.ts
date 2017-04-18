bc40df39f41c8d5c626a462606e13c350b273663
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-04-18 17:04:02 -0300

    Tipos de variaveis

--- src/variables.ts
+++ src/variables.ts
@@ -1,10 +1,51 @@
-// Boolean
+// BOOLEAN
 var boolean:boolean = true;
 
-// Number
+// NUMBER
 var integer:number 	= 2;
 var float:number 	= 5.1;
 var hex:number		= 0xf00d;
 var binary:number 	= 0b1010;
 var octal:number	= 0o744;
 
+// STRING
+var duplas:string = "Jonathan";
+var simples:string = 'Camaro';
+var text = `
+	<p>Texto com quebra de linha (usando crase)</p>
+	<p>duplas: 	${duplas}</p>
+	<p>simples: ${simples}</p>
+`;
+
+// ARRAY
+var fruits:string[] = [
+	"Apple",
+	"Watermelon",
+	"Lemon"
+];
+
+console.log(fruits);
+
+var colors:Array<string> = [
+	"Blue",
+	"Green",
+	"Yellow"
+];
+
+console.log(colors);
+
+// ENUM
+enum Color {Red, Green, Blue}
+
+console.log(Color[0]);
+
+// ANY
+var list:any = 23;
+console.log(list);
+list = "String";
+console.log(list);
+
+// VOID
+function logNumber(num:number):void{
+	alert(num);
+}
